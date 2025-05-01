4d07dbe6119881cad6bd1ef8fc55a13e4fd64416
Sunil-Gurav-0 <guravsunil9482@gmail.com>
2025-05-01 12:04:18 +0530

    upadted

--- src/components/Auth/Singup.jsx
+++ src/components/Auth/Singup.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post(`${process.env.REACT_APP_API_URL}/api/users`, { name, email, password })
+    axios.post("https://jeevan-backend-5shs.vercel.app/api/users", userData)
       .then(result => {
         console.log(result);
         alert('Registration successful! You can now login.'); // Success alert
